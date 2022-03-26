a1f0cd0aa5a8ff0c5509fa6ad4f702eadb488bad
Mevlut Arikan <71103896+mevlutarikan@users.noreply.github.com>
2022-03-26 18:56:33 +0300

    Create odev7.sql
    https://app.patika.dev/courses/sql/Odev7

---
+++ SQL/odev7.sql
@@ -0,0 +1,30 @@
+-- Aşağıdaki sorgu senaryolarını dvdrental örnek veri tabanı üzerinden gerçekleştiriniz
+/*
+1. film tablosunda bulunan filmleri rating değerlerine göre gruplayınız.
+*/
+SELECT rating FROM film
+GROUP BY rating;
+
+/*
+2. film tablosunda bulunan filmleri replacement_cost sütununa göre grupladığımızda film sayısı 50 den fazla olan replacement_cost değerini ve karşılık gelen film sayısını sıralayınız.
+*/
+SELECT replacement_cost,count(*) FROM film
+GROUP BY replacement_cost
+HAVING count(*)>50;
+
+/*
+3. customer tablosunda bulunan store_id değerlerine karşılık gelen müşteri sayılarını nelerdir?
+*/
+SELECT store_id, count(*) FROM customer
+GROUP BY store_id;
+
+/*
+4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
+*/
+
+SELECT country_id, count(*) FROM city
+GROUP BY country_id
+HAVING count(*)=
+(SELECT MAX(count_num) FROM (
+	SELECT country_id, COUNT(*) as count_num FROM city
+	GROUP BY country_id ) as x)
